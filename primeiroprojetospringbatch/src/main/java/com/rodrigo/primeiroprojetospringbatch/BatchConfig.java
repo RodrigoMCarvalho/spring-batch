b0d0cb953891a5f0823af844c15ef2d4a8954bc4
Rodrigo <rodrigo_moreira84@hotmail.com>
2021-12-08 19:47:02 -0300

    Múltiplas execuções e escopo

--- primeiroprojetospringbatch/src/main/java/com/rodrigo/primeiroprojetospringbatch/BatchConfig.java
+++ primeiroprojetospringbatch/src/main/java/com/rodrigo/primeiroprojetospringbatch/BatchConfig.java
@@ -6,10 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,19 +28,29 @@ public class BatchConfig {
 
     @Bean
     public Job imprimeOlaJob() {
-        return jobBuilderFactory.get("imprimeOlaJob").start(imprimeOlaStep()).build();
+        return jobBuilderFactory
+                .get("imprimeOlaJob")
+                .start(imprimeOlaStep())
+                .incrementer(new RunIdIncrementer())  //executar mais de uma vez
+                .build();
     }
 
     public Step imprimeOlaStep() {
         return stepBuilderFactory
                 .get("imprimeOlaStep")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("Olá mundo com Spring Batch.");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
+                .tasklet(imprimeTasket( null)).build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet imprimeTasket(@Value("#{jobParameters['nome']}") String nome) { //Parametro passado pela IDE na execução
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println(String.format("Olá, %s", nome));
+                return RepeatStatus.FINISHED;
+            }
+        };
     }
 }
 
