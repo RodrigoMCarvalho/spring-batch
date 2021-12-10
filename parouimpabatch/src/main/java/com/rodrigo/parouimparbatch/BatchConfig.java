88b9d683fa55e8e0354809d977ffdf805726e86b
Rodrigo <rodrigo_moreira84@hotmail.com>
2021-12-10 19:08:07 -0300

    Criação de um step chunk-based

---
+++ parouimpabatch/src/main/java/com/rodrigo/parouimparbatch/BatchConfig.java
@@ -0,0 +1,65 @@
+package com.rodrigo.parouimparbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job imprimeParOuImparJob() {
+        return jobBuilderFactory
+                .get("imprimeParOuImparJob")
+                .start(imprimeParOuImparStep())
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }   
+
+    public Step imprimeParOuImparStep() {
+        return stepBuilderFactory
+                .get("imprimeParOuImparStep")
+                .<Integer, String>chunk(1)
+                .reader(contaAteDezReader())
+                .processor(parOuImparProcessor())
+                .writer(imprimeWriter())
+                .build();
+    }
+
+    //Reader recebe uma coleção
+    private IteratorItemReader<Integer> contaAteDezReader() {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        return new IteratorItemReader<Integer>(numeros.iterator());
+    }
+
+    //Processor recebe 1 item da coleção de cada vez
+    public FunctionItemProcessor<Integer, String> parOuImparProcessor() {
+        return new FunctionItemProcessor<Integer, String>(item ->
+                item % 2 == 0 ? String.format("Item %s é Par", item) : String.format("Item %s é Impar", item));
+    }
+
+    //Recebe a coleção processada
+    public ItemWriter<String> imprimeWriter () {
+        return itens -> itens.forEach(System.out::println);
+    }
+}
+
---
+++ parouimpabatch/src/main/java/com/rodrigo/parouimparbatch/ParouimparbatchApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.parouimparbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParouimparbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParouimparbatchApplication.class, args);
+	}
+
+}
