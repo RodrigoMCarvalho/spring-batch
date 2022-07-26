552714c30c55a93013ecec4a13c204a77a3d8b88
Rodrigo <rodrigo_moreira84@hotmail.com>
2022-07-26 18:35:19 -0300

    Leitura em banco de dados - JdbcCursorItemReader

--- arquivoMultiplosFormatosJob/src/main/java/com/springbatch/arquivomultiplosformatos/ArquivoMultiplosFormatosJobApplication.java
+++ arquivoMultiplosFormatosJob/src/main/java/com/springbatch/arquivomultiplosformatos/ArquivoMultiplosFormatosJobApplication.java
@@ -4,11 +4,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArquivoMultiplosFormatosJobApplication {
+public class ArquivoMultiplosFormatosJobApplication /*implements CommandLineRunner*/ {
+
+//	@Autowired
+//	private JobLauncher jobLauncher;
+//
+//	@Autowired
+//	private JobExplorer jobExplorer;
+//
+//	@Autowired
+//	private ApplicationContext appContext;
+
+
 
 	//Rodar com o parâmetro "arquivoClientes=file:files/clientes.txt" na IDE
 	public static void main(String[] args) {
 		SpringApplication.run(ArquivoMultiplosFormatosJobApplication.class, args);
 	}
+
+//	@Override
+//	public void run(String... args) throws Exception {
+//		Job job = appContext.getBean("arquivoMultiplosFormatosJob", Job.class);
+//		JobParameters params =
+//				new JobParametersBuilder(jobExplorer).addString("arquivoClientes", "file:files/clientes.txt")
+//						.getNextJobParameters(job)
+//						.toJobParameters();
+//		JobExecution execution = jobLauncher.run(job, params);
+//		System.out.println("STATUS :: " + execution.getStatus());
+//	}
 
 }
