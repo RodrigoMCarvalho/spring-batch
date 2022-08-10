158c2724bb3ef7c305e8aaa41255cdd65295f8fd
Rodrigo <rodrigo_moreira84@hotmail.com>
2022-08-10 19:15:57 -0300

    Escritor com classificadores - ClassifierCompositeItemWriter

--- contasBancariasJob/src/main/java/com/springbatch/contasbancarias/dominio/TipoConta.java
+++ contasBancariasJob/src/main/java/com/springbatch/contasbancarias/dominio/TipoConta.java
@@ -1,9 +1,13 @@
 package com.springbatch.contasbancarias.dominio;
 
+import java.util.Objects;
+
 public enum TipoConta {
-	PRATA, OURO, PLATINA, DIAMANTE;
+	PRATA, OURO, PLATINA, DIAMANTE, INVALIDA;
 	
-	public static TipoConta fromFaixaSalarial(double faixaSalarial) {
+	public static TipoConta fromFaixaSalarial(Double faixaSalarial) {
+		if (Objects.isNull(faixaSalarial))
+			return INVALIDA;
 		if (faixaSalarial <= 3000)
 			return PRATA;
 		else if (faixaSalarial > 3000 && faixaSalarial <= 5000)
